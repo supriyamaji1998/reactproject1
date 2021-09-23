ecd45f658da20e28ef8446c2c1cf471a2664a32f
Supriya Maji <78439168+supriyamaji1998@users.noreply.github.com>
2021-09-23 19:45:24 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,42 +12,50 @@ export default function NavbarBox(props) {
         bg={props.mode === "light" ? "primary" : "dark"}
         variant="dark"
       >
-        {/* <Container> */}
-        {/* <Link to="/">
-          <Navbar.Brand href="/" style={{ marginLeft: "19px" }}>
-            {props.title}
-          </Navbar.Brand>
+        <Link
+          to="/"
+          style={{
+            color: "white",
+            textDecoration: "none",
+            font: "23px",
+            fontWeight: "1000",
+          }}
+        >
           {props.title}
         </Link>
         <Navbar.Toggle
           aria-controls="responsive-navbar-nav"
           style={{ marginRight: "19px" }}
-        /> */}
+        />
         <Navbar.Collapse id="responsive-navbar-nav">
-          {/* <Nav className="me-auto">
-            <Link to="/">
-              <Nav.Link
-                style={{
-                  display: "flex ",
-                  justifyContent: "center",
-                  margin: "0px 19px",
-                }}
-              >
-                {props.navlist1}
-              </Nav.Link>
+          <Nav className="me-auto">
+            <Link
+              to="/"
+              style={{
+                display: "flex ",
+                justifyContent: "center",
+                margin: "0px 19px",
+                color: "white",
+                textDecoration: "none",
+                font: "15px",
+              }}
+            >
+              {props.navlist1}
             </Link>
-            <Link to="/about">
-              <Nav.Link
-                style={{
-                  display: "flex ",
-                  justifyContent: "center",
-                  margin: "0px 19px",
-                }}
-              >
-                {props.navlist2}
-              </Nav.Link>
+            <Link
+              to="/about"
+              style={{
+                display: "flex ",
+                justifyContent: "center",
+                margin: "0px 19px",
+                color: "white",
+                textDecoration: "none",
+                font: "15px",
+              }}
+            >
+              {props.navlist2}
             </Link>
-          </Nav> */}
+          </Nav>
           <Form className="d-flex text-light" style={{ margin: "0px 19px" }}>
             <Form>
               <Form.Check
@@ -59,7 +67,6 @@ export default function NavbarBox(props) {
             </Form>
           </Form>
         </Navbar.Collapse>
-        {/* </Container> */}
       </Navbar>
     </div>
   );

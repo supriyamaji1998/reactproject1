4140ba8ea4703f3963a149f5b22b75ce96bc0a6a
Supriya Maji <78439168+supriyamaji1998@users.noreply.github.com>
2021-09-23 20:18:37 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,12 @@ import React from "react";
 export default function About() {
   return (
     <div>
-      <h1>this is About</h1>
+      <h3>
+        I am final year student of computer science and engineering. I completed
+        my Diploma in computer science and technology from Acharya Prafulla
+        Chandra Ray Polytechnic, Kolkata. Data structure and algorithm is my
+        strength, I have knowledge about Java and JavaScript and React.js.
+      </h3>
     </div>
   );
 }
